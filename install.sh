34766c111930d2f5b3b52d9acee35263deab44e0
tredel <tredel@rohling.test.net>
2010-09-10 15:58:22 +0200

    * prefixes for lconf schema can now be set dynamically

--- install.sh
+++ install.sh
@@ -59,39 +59,43 @@ install -d -o $USER -g $GROUP -m 750 $PREFIX/etc
 install -d -o $USER -g $GROUP -m 750 $PREFIX/var
 
 ScriptOutput CREATE "LConf Exporter"
-cat $DIR_SOURCE/LConfExport.pl.in | sed s/@prefix@/$PREFIX_QUOTED/ > $DIR_SOURCE/LConfExport.pl
+cat $DIR_SOURCE/LConfExport.pl.in | sed -e "s/@prefix@/$PREFIX_QUOTED/g" > $DIR_SOURCE/LConfExport.pl.tmp1
+cat $DIR_SOURCE/LConfExport.pl.tmp1 | sed -e "s/@ldapprefix@/$LDAPPREFIX/g" > $DIR_SOURCE/LConfExport.pl
 ScriptOutput INSTALL "LConf Exporter"
 install -o $USER -g $GROUP -m 750 $DIR_SOURCE/LConfExport.pl $PREFIX/
 
 ScriptOutput CREATE "LConf Importer"
-cat $DIR_SOURCE/LConfImport.pl.in | sed s/@prefix@/$PREFIX_QUOTED/ > $DIR_SOURCE/LConfImport.pl
+cat $DIR_SOURCE/LConfImport.pl.in | sed -e "s/@prefix@/$PREFIX_QUOTED/g" > $DIR_SOURCE/LConfImport.pl.tmp1
+cat $DIR_SOURCE/LConfImport.pl.tmp1 | sed -e "s/@ldapprefix@/$LDAPPREFIX/g" > $DIR_SOURCE/LConfImport.pl
 ScriptOutput INSTALL "LConf Importer"
 install -o $USER -g $GROUP -m 750 $DIR_SOURCE/LConfImport.pl $PREFIX/
 
 ScriptOutput CREATE "LConf Slave Exporter"
-cat $DIR_SOURCE/LConfSlaveExport.pl.in | sed s/@prefix@/$PREFIX_QUOTED/ > $DIR_SOURCE/LConfSlaveExport.pl
+cat $DIR_SOURCE/LConfSlaveExport.pl.in | sed -e "s/@prefix@/$PREFIX_QUOTED/g" > $DIR_SOURCE/LConfSlaveExport.pl.tmp1
+cat $DIR_SOURCE/LConfSlaveExport.pl.tmp1 | sed -e "s/@ldapprefix@/$LDAPPREFIX/g" > $DIR_SOURCE/LConfSlaveExport.pl
 ScriptOutput INSTALL "LConf Slave Exporter"
 install -o $USER -g $GROUP -m 750 $DIR_SOURCE/LConfSlaveExport.pl $PREFIX/
 
 ScriptOutput CREATE "LConf Slave Syncer"
-cat $DIR_SOURCE/LConfSlaveSync.pl.in | sed s/@prefix@/$PREFIX_QUOTED/ > $DIR_SOURCE/LConfSlaveSync.pl
+cat $DIR_SOURCE/LConfSlaveSync.pl.in | sed -e "s/@prefix@/$PREFIX_QUOTED/g" > $DIR_SOURCE/LConfSlaveSync.pl
 ScriptOutput INSTALL "LConf Slave Syncer"
 install -o $USER -g $GROUP -m 750 $DIR_SOURCE/LConfSlaveSync.pl $PREFIX/
 
 ScriptOutput CREATE "LConf config file"
-cat $DIR_SOURCE/config.pm.in | sed s/@domain@/$ROOTDN/ > $DIR_SOURCE/config.pm.tmp1
-cat $DIR_SOURCE/config.pm.tmp1 | sed s/@user@/$USER/ > $DIR_SOURCE/config.pm.tmp2
-cat $DIR_SOURCE/config.pm.tmp2 | sed s/@prefix@/$PREFIX_QUOTED/ > $DIR_SOURCE/config.pm
+cat $DIR_SOURCE/config.pm.in | sed -e "s/@domain@/$ROOTDN/g" > $DIR_SOURCE/config.pm.tmp1
+cat $DIR_SOURCE/config.pm.tmp1 | sed -e "s/@user@/$USER/g" > $DIR_SOURCE/config.pm.tmp2
+cat $DIR_SOURCE/config.pm.tmp2 | sed -e "s/@prefix@/$PREFIX_QUOTED/g" > $DIR_SOURCE/config.pm
 ScriptOutput INSTALL "LConf config file"
 install -o $USER -g $GROUP -m 700 $DIR_SOURCE/config.pm $PREFIX/etc/
 
 ScriptOutput INSTALL "LDAP schema file"
+cat $DIR_SOURCE/netways.schema.in | sed -e "s/@ldapprefix@/$LDAPPREFIX/g" > $DIR_SOURCE/netways.schema
 install -o root -g root -m 644 $DIR_SOURCE/netways.schema $DIR_SCHEMA/
 
 ScriptOutput INSTALL "Default templates"
 install -o $USER -g $GROUP -m 640 $DIR_SOURCE/default-templates.cfg $PREFIX/etc/
 
 ScriptOutput CREATE "LDAP base ldif"
-cat $DIR_SOURCE/base.ldif.in | sed s/@domain@/$ROOTDN/ > $DIR_SOURCE/base.ldif
+cat $DIR_SOURCE/base.ldif.in | sed -e "s/@domain@/$ROOTDN/g" > $DIR_SOURCE/base.ldif
 
 WizardManualy
