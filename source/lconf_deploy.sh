cb428fc6746cd4fe713735c430bf8f3f1e7d9422
Birger Schmidt <birger.schmidt@netways.de>
2011-04-05 17:24:05 +0200

    * check config in a tmp dir before overwriting the previous config

--- source/lconf_deploy.sh
+++ source/lconf_deploy.sh
@@ -8,6 +8,14 @@
 SATELLIT="satellit1 satellit2 satellit3"
 LCONFDIR="/etc/icinga/lconf"
 LCONFTMP="/etc/icinga/lconf.tmp"
+ICINGACONFIG=/etc/icinga/icinga.cfg
+ICINGATMPCONFIG=/etc/icinga/icinga.tmp.cfg
+
+RUNUSER=$(whoami)
+SUDOCOMMAND=""
+if [ "$RUNUSER" != "icinga" ] ; then
+  SUDOCOMMAND="sudo -u icinga"
+fi
 
 
 #################################################
@@ -18,33 +26,41 @@ install -d -o icinga -g icinga $LCONFDIR
 install -d -o icinga -g icinga $LCONFTMP
 
 
+# alter the icinga.cfg to check against the tmp dir
+install -o icinga -g icinga -m 640 $ICINGACONFIG $ICINGATMPCONFIG
+sed 's/^\(cfg_.*\)/#lconftest#\1/' $ICINGACONFIG > $ICINGATMPCONFIG
+echo cfg_dir=$LCONFTMP >> $ICINGATMPCONFIG
+
+
 echo export config from LDAP
 # export original full config from LDAP
-(cd /usr/local/LConf/ ; sudo -u icinga ./LConfExport.pl -o $LCONFTMP)
-
-# copy the preliminary config in place to pass the check
-rsync -a --del "$LCONFTMP"/* "$LCONFDIR"
-
-# generate config for satellites
-# this process may alter the original config
-# (disable checks of satellite components on the master...)
-if ( icinga -v /etc/icinga/icinga.cfg ) then
-  
+(cd /usr/local/LConf/ ; $SUDOCOMMAND ./LConfExport.pl -o $LCONFTMP)
+ 
+ 
+# first test the config within the tmp dir
+if ( icinga -v $ICINGATMPCONFIG ) then
+ 
+  # copy the preliminary config in place to pass the check
+  $SUDOCOMMAND rsync -a --del "$LCONFTMP"/* "$LCONFDIR"
+ 
+  # generate config for satellites
+  # this process may alter the original config
+  # (disable checks of satellite components on the master...)
   for HOST in $SATELLIT ; do
     echo deploy config on $HOST
     (cd /usr/local/LConf/ ; \
-     sudo -u icinga \
+     $SUDOCOMMAND \
      ./LConfSlaveExport.pl -H $HOST \
      -s $LCONFTMP \
      -t $LCONFDIR -v )
   done
 
   # copy the final config in place
-  rsync -a --del "$LCONFTMP"/* "$LCONFDIR"
+  $SUDOCOMMAND rsync -a --del "$LCONFTMP"/* "$LCONFDIR"
 
   # reload the final config on the master
   echo reload config on Master $(hostname -f)
-  /etc/init.d/icinga reload
+  $SUDOCOMMAND /etc/init.d/icinga reload
 
 fi
 
