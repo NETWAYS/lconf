598f4448ea6d44dac429a3e676c22bc6520d5974
Birger Schmidt <birger.schmidt@netways.de>
2011-04-14 11:19:24 +0200

    * lconf_deploy.sh: use vars to make it configurable, fix a rsync/sudo issue

--- source/lconf_deploy.sh
+++ source/lconf_deploy.sh
@@ -10,11 +10,23 @@ LCONFDIR="/etc/icinga/lconf"
 LCONFTMP="/etc/icinga/lconf.tmp"
 ICINGACONFIG=/etc/icinga/icinga.cfg
 ICINGATMPCONFIG=/etc/icinga/icinga.tmp.cfg
+ICINGAUSER=icinga
+ICINGAGROUP=icinga
+ICINGABIN=$(which icinga)
+
+
+if [ ! -x $ICINGABIN ] ; then
+  ICINGABIN=/usr/local/icinga/bin/icinga
+  if [ ! -x $ICINGABIN ] ; then
+    echo "ERROR: Could not find icinga binary to check the config!"
+    exit 2
+  fi
+fi
 
 RUNUSER=$(whoami)
 SUDOCOMMAND=""
-if [ "$RUNUSER" != "icinga" ] ; then
-  SUDOCOMMAND="sudo -u icinga"
+if [ "$RUNUSER" != $ICINGAUSER ] ; then
+  SUDOCOMMAND="sudo -u $ICINGAUSER"
 fi
 
 
@@ -22,12 +34,12 @@ fi
 # main
 #################################################
 
-install -d -o icinga -g icinga $LCONFDIR
-install -d -o icinga -g icinga $LCONFTMP
+install -d -o $ICINGAUSER -g $ICINGAGROUP $LCONFDIR
+install -d -o $ICINGAUSER -g $ICINGAGROUP $LCONFTMP
 
 
 # alter the icinga.cfg to check against the tmp dir
-install -o icinga -g icinga -m 640 $ICINGACONFIG $ICINGATMPCONFIG
+install -o $ICINGAUSER -g $ICINGAGROUP -m 640 $ICINGACONFIG $ICINGATMPCONFIG
 sed 's/^\(cfg_.*\)/#lconftest#\1/' $ICINGACONFIG > $ICINGATMPCONFIG
 echo cfg_dir=$LCONFTMP >> $ICINGATMPCONFIG
 
@@ -38,10 +50,7 @@ echo export config from LDAP
  
  
 # first test the config within the tmp dir
-if ( icinga -v $ICINGATMPCONFIG ) then
- 
-  # copy the preliminary config in place to pass the check
-  $SUDOCOMMAND rsync -a --del "$LCONFTMP"/* "$LCONFDIR"
+if ( $ICINGABIN -v $ICINGATMPCONFIG ) then
  
   # generate config for satellites
   # this process may alter the original config
@@ -56,7 +65,7 @@ if ( icinga -v $ICINGATMPCONFIG ) then
   done
 
   # copy the final config in place
-  $SUDOCOMMAND rsync -a --del "$LCONFTMP"/* "$LCONFDIR"
+  $SUDOCOMMAND -i rsync -a --del "$LCONFTMP"/ "$LCONFDIR"
 
   # reload the final config on the master
   echo reload config on Master $(hostname -f)
